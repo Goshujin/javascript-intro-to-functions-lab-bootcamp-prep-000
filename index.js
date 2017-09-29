f4abc0625ca98567c057dbc7bad9ba8f677e535b
Iwan Pattikawa <iwanpattikawa95@hotmail.com>
2017-09-29 14:05:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(string) {
   console.log('hello')
 }
 
-function sayHiToGrandma(.toUpperCase(string)) {
+function sayHiToGrandma.toUpperCase(string) {
   return `I can't hear you!`
 }
